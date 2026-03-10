b62c9f6630af6631440ad12d157f7edaf242ee15
Voetbal4All <voetbal4all@outlook.com>
2026-03-10 19:44:37 +0100

    fix(scout): correct JSX closing tags in Card

--- src/scout/components/ui/Card.jsx
+++ src/scout/components/ui/Card.jsx
@@ -2,27 +2,27 @@ import React from 'react';
 import styles from './Card.module.css';
 
 const Card = ({
-    hoverable = false,
-    glowColor = 'blue',
-    noPadding = false,
-    children,
-    className = '',
-    ...props
+  hoverable = false,
+  glowColor = 'blue',
+  noPadding = false,
+  children,
+  className = '',
+  ...props
 }) => {
-    const classNames = [
-          styles.card,
-          hoverable ? styles['card--hoverable'] : '',
-          styles[`card--glow-${glowColor}`],
-          noPadding ? styles['card--no-padding'] : '',
-          className,
-        ].filter(Boolean).join(' ');
+  const classNames = [
+    styles.card,
+    hoverable ? styles['card--hoverable'] : '',
+    styles[`card--glow-${glowColor}`],
+    noPadding ? styles['card--no-padding'] : '',
+    className,
+  ].filter(Boolean).join(' ');
 
-    return (
-          <div className={classNames} {...props}>
-            {hoverable && <div className={styles.glowLine} />}
-            {children}
-          </div>div>
-        );
+  return (
+    <div className={classNames} {...props}>
+      {hoverable && <div className={styles.glowLine} />}
+      {children}
+    </div>
+  );
 };
 
-export default Card;</div>
+export default Card;
